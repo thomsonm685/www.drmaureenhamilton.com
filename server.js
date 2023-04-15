445386a3137155096bfdb9ccc9f13698548a977f
Michael Thomson <michael@takoagency.com>
2023-04-14 21:03:55 -0500

    server

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static('public'))
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
-});
+}); 
 
 app.get('/events', function(req, res) {
     res.sendFile(path.join(__dirname, '/front-end/www.brysonstavern.com/events.html'));
